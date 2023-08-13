bebe27ee0e58b461f49f6306f3e5b131af971f4e
Ahnaf Anan <aanana0171@gmail.com>
2023-08-13 23:29:31 +0600

    onMounted

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import DataFetchingComponent from './components/DataFetchingComponent.vue';
+
+</script>
+
+<template>
+  <DataFetchingComponent />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/DataFetchingComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onBeforeMount, onMounted } from 'vue';
+
+const apiResponse = ref(null);
+
+onMounted(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then((response) => response.json())
+        .then((data) => {
+            apiResponse.value = data;
+        });
+});
+
+</script>
+
+<template>
+    <div class="my-5 fs-4">{{ apiResponse }}</div>
+    <div class="text-start fs-3" v-for="(value, key, index) in apiResponse" :key="index">
+        {{ key }}: {{ value }}
+    </div>
+</template>
+
+<style scoped></style>
